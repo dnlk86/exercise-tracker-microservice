a247559014df25c51674843d14ca4a7de0d39b7f
Daniel Keicher <dnlkch86@gmail.com>
2023-03-11 17:52:28 +0100

    feat: mongodb connection

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,8 +11,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Mongo db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "exercise-tracker" })
+  .then(() => {
+    console.log(`CONNECTED TO MONGO!`);
+  })
+  .catch((err) => {
+    console.log(`MONGO CONNECTION ERROR!`);
+    console.log(err);
+  });
 
-
+// Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
