229cbc023ee30f36ac93df25f41caba2ceb89cb2
Daniel Keicher <dnlkch86@gmail.com>
2023-03-12 10:23:49 +0100

    feat: complete users POST

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 // Body-parser middleware
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Mongo db connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "exercise-tracker" })
-  .then(() => {
-    console.log(`CONNECTED TO MONGO!`);
-  })
-  .catch((err) => {
-    console.log(`MONGO CONNECTION ERROR!`);
-    console.log(err);
-  });
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "exercise-tracker",
+    })
+    .then(() => {
+        console.log(`CONNECTED TO MONGO!`);
+    })
+    .catch((err) => {
+        console.log(`MONGO CONNECTION ERROR!`);
+        console.log(err);
+    });
 
 // Models import
 const User = require("./src/models/user");
 const Session = require("./src/models/session");
 
 // Create user
-app.post("/api/users", function(req, res) {
-  let userName = req.body.username;
-  if (!userName) {
-    res.json({ error: "insert a username" });
-  } else {
-    // inser username in db
-    let user = new User({ username: userName });
-    user.save().then(data => {
-      if (!data) {
-        console.log("error: record not saved!");
-      } else {
-        res.json({ "username": data["username"] });
-      }
-    });
-  }
+app.post("/api/users", function (req, res) {
+    let userName = req.body.username;
+    if (!userName) {
+        res.json({ error: "insert a username" });
+    } else {
+        // check if username already exist in db
+        User.findOne({ username: userName }).then((retrievedUser) => {
+            if (!retrievedUser) {
+                // insert username in db
+                let user = new User({ username: userName });
+                user.save().then((data) => {
+                    if (!data) {
+                        console.log("error: record not saved!");
+                    } else {
+                        res.json({
+                            username: data["username"],
+                            _id: data["_id"],
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    username: retrievedUser["username"],
+                    _id: retrievedUser["_id"],
+                });
+            }
+        });
+    }
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
